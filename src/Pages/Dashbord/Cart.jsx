cad25acdcf08c90ce9285522afad6c32aeaa1f6e
salman-farcy <salmanfarcy253@gmail.com>
2023-12-28 09:24:18 +0600

    add card Ui desing

--- src/Pages/Dashbord/Cart.jsx
+++ src/Pages/Dashbord/Cart.jsx
@@ -1,9 +1,16 @@
+import useCart from "../../Hooks/useCart";
 
 
 const Cart = () => {
+     const [cart] = useCart();
+     
      return (
           <div>
-               <h2 className='text-6xl'>My Cart</h2>
+              <div className="">
+                  <h2>Items: {cart.length}</h2>
+                  <h2>Items: {totalPrice}</h2>
+                  <button>Pay</button>
+               </div>
           </div>
      );
 };
