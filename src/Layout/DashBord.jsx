3f555f2016b86d78aa03280efb02cdd68812745e
salman-farcy <salmanfarcy253@gmail.com>
2023-12-30 19:42:37 +0600

    Card indexing and design updates

--- src/Layout/DashBord.jsx
+++ src/Layout/DashBord.jsx
@@ -6,76 +6,84 @@ import { MdOutlineReorder } from "react-icons/md";
 import { FaPersonSkiingNordic } from "react-icons/fa6";
 import { Helmet } from "react-helmet-async";
 import { AiFillHome } from "react-icons/ai";
+import SectionTitel from "../Components/SectionTitel";
 
 
 
 
 const DashBord = () => {
      return (
-          <div className="flex ">
+          <div className=" ">
                <Helmet>
                     <title>Bistro Boss | Dashbord</title>
                </Helmet>
 
-               <div className="w-64  min-h-screen bg-orange-300">
-                    <ul className="menu">
-                         <li>
-                              <NavLink to="/dashbord/userhome">
-                                   <AiFillHome fontSize={30} className="mr-2" /> User Home
-                              </NavLink>
-                         </li>
+               <div className="flex  bg-gray-50">
+                    <div className="w-64  min-h-screen bg-red-100">
+                         <ul className="menu">
+                              <li>
+                                   <NavLink to="/dashbord/userhome">
+                                        <AiFillHome fontSize={30} className="mr-2" /> User Home
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/dashbord/reservation">
-                                   <FaCalendar fontSize={30} className="mr-2" /> Reservation 
-                              </NavLink>
-                         </li>
+                              <li>
+                                   <NavLink to="/dashbord/reservation">
+                                        <FaCalendar fontSize={30} className="mr-2" /> Reservation
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/dashbord/cart">
-                                   <FaCartShopping fontSize={30} className="mr-2" /> My Cart
-                              </NavLink>
-                         </li>
+                              <li>
+                                   <NavLink to="/dashbord/cart">
+                                        <FaCartShopping fontSize={30} className="mr-2" /> My Cart
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/dashbord/review">
-                                   <FaSplotch fontSize={30} className="mr-2"  /> Add Review
-                              </NavLink>
-                         </li>
+                              <li>
+                                   <NavLink to="/dashbord/review">
+                                        <FaSplotch fontSize={30} className="mr-2" /> Add Review
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/dashbord/bookings">
-                                   <BsFillJournalBookmarkFill fontSize={30} className="mr-2"  /> My Bookings
-                              </NavLink>
-                         </li>
-                         
-                         <div className="divider"></div>
+                              <li>
+                                   <NavLink to="/dashbord/bookings">
+                                        <BsFillJournalBookmarkFill fontSize={30} className="mr-2" /> My Bookings
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/">
-                                   <AiFillHome fontSize={30} className="mr-2" /> Home
-                              </NavLink>
-                         </li>
+                              <div className="divider"></div>
 
-                         <li>
-                              <NavLink to="/order/salad">
-                                   <FaPersonSkiingNordic fontSize={30} className="mr-2" /> Order
-                              </NavLink>
-                         </li>
+                              <li>
+                                   <NavLink to="/">
+                                        <AiFillHome fontSize={30} className="mr-2" /> Home
+                                   </NavLink>
+                              </li>
 
-                         <li>
-                              <NavLink to="/menu">
-                                   <MdOutlineReorder fontSize={30} className="mr-2" /> Menu
-                              </NavLink>
-                         </li>
+                              <li>
+                                   <NavLink to="/order/salad">
+                                        <FaPersonSkiingNordic fontSize={30} className="mr-2" /> Order
+                                   </NavLink>
+                              </li>
 
-                    </ul>
+                              <li>
+                                   <NavLink to="/menu">
+                                        <MdOutlineReorder fontSize={30} className="mr-2" /> Menu
+                                   </NavLink>
+                              </li>
+
+                         </ul>
+                    </div>
+
+                    <div className="flex-1 px-5 ">
+                         <div className="bg-red-100 mb-5">
+                              <SectionTitel subHeading={"---My Cart---"} heading={"WANNA ADD MORE?"}></SectionTitel>
+                         </div>
+
+                         <div className="">
+                              <Outlet></Outlet>
+                         </div>
+                    </div>
                </div>
-
-               <div className="flex-1">
-                    <Outlet></Outlet>
-               </div>
-
           </div>
      );
 };
--- src/Pages/Dashbord/Cart.jsx
+++ src/Pages/Dashbord/Cart.jsx
@@ -1,15 +1,72 @@
+import { FaDeleteLeft, FaTrash } from "react-icons/fa6";
 import useCart from "../../Hooks/useCart";
 
 
 const Cart = () => {
      const [cart] = useCart();
-     const totalPrice = cart.reduce((total, item) => total + item.price, 0 )
+     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
      return (
-          <div>
-              <div className="">
-                  <h2>Items: {cart.length}</h2>
-                  <h2>Items: {totalPrice}</h2>
-                  <button>Pay</button>
+          <div className="bg-red-100">
+               <div className="flex justify-between p-5">
+                    <h2 className="text-3xl font-semibold">Items: {cart.length}</h2>
+                    <h2 className="text-3xl font-semibold">Items: {totalPrice}</h2>
+                    <button className=" bg-red-100 px-5 py-2 hover:bg-red-200">Pay</button>
+               </div>
+
+               <div className="overflow-x-auto  ">
+                    <table className="table ">
+                         {/* head */}
+                         <thead className="bg-red-200">
+                              <tr>
+                                   {/* <th>
+                                        <label>
+                                             <input type="checkbox" className="checkbox" />
+                                        </label>
+                                   </th> */}
+                                   <th>Name</th>
+                                   <th>Job</th>
+                                   <th>Favorite Color</th>
+                                   <th>Dtail</th>
+                                   <th>Trast</th>
+                              </tr>
+                         </thead>
+
+                         <tbody>
+                              {
+                                   cart.map(( item, index ) => <tr className="overflow-y-hidden" key={item._id}>
+                                        <th>
+                                             {index + 1}
+                                        </th>
+                                        <td>
+                                             <div className="flex items-center gap-3">
+                                                  <div className="avatar">
+                                                       <div className="mask  w-12 h-12">
+                                                            <img src={item.image} />
+                                                       </div>
+                                                  </div>
+                                                  <div>
+                                                       <div className="font-bold">{item?.name}</div>
+                                                       <div className="text-sm opacity-50 font-bold">${item?.price}</div>
+                                                  </div>
+                                             </div>
+                                        </td>
+                                        <td>
+                                             Zemlak, Daniel and Leannon
+                                             <br />
+                                             <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                        </td>
+                                        <td>Purple</td>
+                                        <th>
+                                             <button className="btn btn-ghost btn-md">
+                                                  <FaTrash className="text-red-500"></FaTrash>
+                                             </button>
+                                        </th>
+                                   </tr>)
+                              }
+                              
+                              
+                         </tbody>
+                    </table>
                </div>
           </div>
      );
