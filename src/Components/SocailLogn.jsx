9e5ac24f133efff77ccb543c41e46d7ea322f2a8
salman-farcy <salmanfarcy253@gmail.com>
2024-01-01 22:14:04 +0600

    The system of keeping the user's data in the database by logging in through Google

--- src/Components/SocailLogn.jsx
+++ src/Components/SocailLogn.jsx
@@ -1,14 +1,28 @@
 import { FcGoogle } from "react-icons/fc";
 import useAuth from "../Hooks/useAuth";
+import useAxiosPublic from "../Hooks/axiosPublickHook/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 
 const SocailLogn = () => {
      const { googleSignIn } = useAuth();
+     const axiosPublic = useAxiosPublic()
+     const navigate = useNavigate()
 
      const handleGoogleSignIn = () => {
           googleSignIn()
           .then(result => {
-               console.log(result.user);
+               console.log(result.user.email);
+               const userInfo = { 
+                    email: result?.user?.email,
+                    name: result?.user?.displayName
+               }
+
+               axiosPublic.post('/users', userInfo)
+               .then(res => {
+                    console.log(res.data);
+                    navigate('/')
+               })
           })
      }
  
