1a8b69d0e0c34dd939eb8d46e45062147e7a5904
salman-farcy <salmanfarcy253@gmail.com>
2024-02-01 14:59:43 +0600

    CustomShapeBarChart add admin home page

--- src/Pages/Dashbord/Admin/AdminHome.jsx
+++ src/Pages/Dashbord/Admin/AdminHome.jsx
@@ -6,6 +6,9 @@ import { FaUsers } from "react-icons/fa6";
 import { LuChefHat } from "react-icons/lu";
 import { CiDeliveryTruck } from "react-icons/ci";
 
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink', 'green'];
+
 
 const AdminHome = () => {
      const { user } = useAuth()
@@ -19,7 +22,7 @@ const AdminHome = () => {
           }
      })
 
-     const {data: cartData = []} = useQuery({
+     const { data: cartData = [] } = useQuery({
           queryKey: ['order-stats'],
           queryFn: async () => {
                const res = await axiosSecure.get('/order-stats')
@@ -27,8 +30,19 @@ const AdminHome = () => {
           }
      })
 
+     // Custom shape for the bar chart
+     const getPath = (x, y, width, height) => {
+          return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+          ${x + width / 2}, ${y}
+          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+          Z`;
+     };
 
-     
+     const TriangleBar = (props) => {
+          const { fill, x, y, width, height } = props;
+
+          return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+     };
 
      return (
           <div className="text-3xl font-semibold">
@@ -89,7 +103,35 @@ const AdminHome = () => {
                     </div>
                </div>
 
-               
+               <div className="flex gap-5">
+                    {/* chart 01 */}
+                    <div className="">
+                    
+                         <BarChart
+                              width={800}
+                              height={500}
+                              data={cartData}
+                              margin={{
+                                   top: 50,
+                                   right: 30,
+                                   left: 20,
+                                   bottom: 5,
+                              }}
+                         >
+                              <CartesianGrid strokeDasharray="3 3" />
+                              <XAxis dataKey="category" />
+                              <YAxis />
+                              <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                                   {cartData.map((entry, index) => (
+                                        <Cell key={`cell-${index}`} fill={colors[index % 7]} />
+                                   ))}
+                              </Bar>
+                         </BarChart>
+                         
+                    </div>
+
+                    {/* chart 02 */}
+               </div>
           </div>
 
 
